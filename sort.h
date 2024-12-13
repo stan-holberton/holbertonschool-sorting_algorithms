ccb1b8b231d9b39fa37b47d6d84b7f3324b4e179
stan-holberton <9857@holbertonstudents.com>
2024-12-13 09:31:29 +0000

    Mise a jour du fichier .h avec le proto pour la task 0

---
+++ sort.h
@@ -0,0 +1,27 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+
+/**
+ * struct listint_s - Structure d'une liste doublement chaînée
+ * @n: Donnée de l'élément
+ * @prev: Pointeur vers le précédent élément de la liste
+ * @next: Pointeur vers le suivant élément de la liste
+ *
+ * Description: Structure représentant un nœud dans une liste
+ * doublement chaînée utilisée pour certains algorithmes de tri.
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* Prototypes des fonctions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+
+#endif /* SORT_H */
