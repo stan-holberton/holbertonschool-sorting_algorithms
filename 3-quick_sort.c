0c471afe957cd0b944de83a9e5972e0d0a2b1632
stan-holberton <9857@holbertonstudents.com>
2024-12-13 16:13:00 +0000

    Mise a jour du fichier 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,22 +1,23 @@
-#include <stdio.h>
 #include "sort.h"
+#include <stdio.h>
 
 /**
- * partition - Effectue la partition autour du pivot.
+ * partition - Partitionne un tableau en fonction d'un pivot.
  * @array: Le tableau à trier.
  * @low: L'indice bas de la partition.
  * @high: L'indice haut de la partition.
+ * @size: La taille du tableau.
  *
- * Return: L'indice du pivot.
+ * Return: L'indice du pivot après partitionnement.
  */
-int partition(int *array, int low, int high)
+int partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
 	int i = low - 1;
-	int temp;
 	int j;
+	int temp;
 
-	for (j = low; j < high; j++)
+	for (j = low; j <= high - 1; j++)
 	{
 		if (array[j] <= pivot)
 		{
@@ -24,36 +25,40 @@ int partition(int *array, int low, int high)
 			temp = array[i];
 			array[i] = array[j];
 			array[j] = temp;
+			if (i != j)
+				print_array(array, size);
 		}
 	}
-
 	temp = array[i + 1];
 	array[i + 1] = array[high];
 	array[high] = temp;
-	print_array(array, high - low + 1);
+	if (i + 1 != high)
+		print_array(array, size);
 
 	return (i + 1);
 }
 
 /**
- * quick_sort_recursive - Fonction récursive pour appliquer le tri rapide.
+ * quick_sort_recursive - Effectue un tri rapide récursif sur un tableau.
  * @array: Le tableau à trier.
  * @low: L'indice bas de la sous-partie du tableau.
  * @high: L'indice haut de la sous-partie du tableau.
+ * @size: La taille du tableau.
  */
-void quick_sort_recursive(int *array, int low, int high)
+void quick_sort_recursive(int *array, int low, int high, size_t size)
 {
 	if (low < high)
 	{
-		int pivot_index = partition(array, low, high);
+		int pivot_index = partition(array, low, high, size);
 
-		quick_sort_recursive(array, low, pivot_index - 1);
-		quick_sort_recursive(array, pivot_index + 1, high);
+		quick_sort_recursive(array, low, pivot_index - 1, size);
+		quick_sort_recursive(array, pivot_index + 1, high, size);
 	}
 }
 
 /**
- * quick_sort - Fonction principale pour trier le tableau.
+ * quick_sort - Trie un tableau d'entiers en ordre croissant avec l'algorithme
+ * de tri rapide.
  * @array: Le tableau à trier.
  * @size: La taille du tableau.
  */
@@ -62,5 +67,5 @@ void quick_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	quick_sort_recursive(array, 0, size - 1);
+	quick_sort_recursive(array, 0, size - 1, size);
 }
